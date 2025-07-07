2e9570223c982685eb1c5979d40f219541c2f2c7
Matteo Tinta <matteo.tinta.external@ynap.com>
2025-07-07 16:45:20 +0200

    feat: added internalize headless to architecture

--- start.ps1
+++ start.ps1
@@ -1,6 +1,6 @@
 docker compose `
   -f ./compose.yml `
   -f ./services/auth/compose.yml `
-  -f ./services/internalize/compose.yml `
+  -f ./services/internalize/compose.headless.yml `
   -f ./services/persistence/compose.yml `
   up -d
