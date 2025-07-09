9a677d79f1bd32cc5b6ae23590e74c9775930444
Matteo Tinta <matteo.tinta.external@ynap.com>
2025-07-09 19:44:28 +0200

    feat: added query service

--- start.ps1
+++ start.ps1
@@ -1,5 +1,6 @@
 docker compose `
   -f ./compose.yml `
+  -f ./services/query/compose.yml `
   -f ./services/auth/compose.yml `
   -f ./services/internalize/compose.headless.yml `
   -f ./services/persistence/compose.yml `
